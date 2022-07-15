95201875b392748f2d2c192b54443ea2187a565f
donghune <reona7140@naver.com>
2022-07-15 17:11:11 +0900

    버그수정

--- app.py
+++ app.py
@@ -9,32 +9,16 @@ CORS(app, resources={r'*': {'origins': 'http://152.70.248.4'}})
 
 @app.route("/basic/<string:nickname>/")
 def basic(nickname):
-    return lostark.get_character_info_all(nickname)["basic"]
-
-
-@app.route("/level/<string:nickname>/")
-def level(nickname):
-    return lostark.get_character_info_all(nickname)["level"]
+    return lostark.get_character_info_all(nickname)["basic"] + " |  | " + \
+           lostark.get_character_info_all(nickname)["level"] + " |  | " + \
+           lostark.get_character_info_all(nickname)["stat"] + " |  | " + \
+           lostark.get_character_info_all(nickname)["engrave"] + " |  | " + \
+           lostark.get_character_info_all(nickname)["collections"]
 
 
 @app.route("/sub_character/<string:nickname>/")
 def sub_character(nickname):
     return lostark.get_character_info_all(nickname)["sub_character"]
-
-
-@app.route("/stat/<string:nickname>/")
-def stat(nickname):
-    return lostark.get_character_info_all(nickname)["stat"]
-
-
-@app.route("/engrave/<string:nickname>/")
-def engrave(nickname):
-    return lostark.get_character_info_all(nickname)["engrave"]
-
-
-@app.route("/ability_stone/<string:nickname>/")
-def ability_stone(nickname):
-    return lostark.get_character_info_all(nickname)["ability_stone"]
 
 
 @app.route("/jewelry/<string:nickname>/")
@@ -47,24 +31,20 @@ def equipment(nickname):
     return lostark.get_character_info_all(nickname)["equipment"]
 
 
+@app.route("/accessories/<string:nickname>/")
+def accessories(nickname):
+    return lostark.get_character_info_all(nickname)["accessories"] + " |  | " + \
+           lostark.get_character_info_all(nickname)["ability_stone"]
+
+
 @app.route("/week_gold/<string:nickname>/")
 def week_gold(nickname):
     return lostark.get_character_info_all(nickname)["week_gold"]
 
 
-@app.route("/accessories/<string:nickname>/")
-def accessories(nickname):
-    return lostark.get_character_info_all(nickname)["accessories"]
-
-
 @app.route("/skill/<string:nickname>/")
 def skill(nickname):
     return lostark.get_character_info_all(nickname)["skill"]
-
-
-@app.route("/collections/<string:nickname>/")
-def collections(nickname):
-    return lostark.get_character_info_all(nickname)["collections"]
 
 
 if __name__ == "__main__":
