f8c7c2a3b6e418b1e8d1a75d90db363f30a9abcd
donghune <reona7140@naver.com>
2022-07-15 17:56:16 +0900

    ㅁㄴㅇ

--- api/lostark.py
+++ api/lostark.py
@@ -3,69 +3,56 @@ import requests
 from api import lostark_parser
 
 
-def get_character_info_all(username):
+def get_basic(username):
     res = requests.get('http://152.70.248.4:5000/userinfo/' + username)
     data = res.json()
-    return {
-        "basic": lostark_parser.parse_basic(data),
-        "level": lostark_parser.parse_level(data),
-        "sub_character": lostark_parser.parse_sub_character(data),
-        "stat": lostark_parser.parse_stat(data),
-        "engrave": lostark_parser.parse_engrave(data),
-        "ability_stone": lostark_parser.parse_ability_stone(data),
-        "jewelry": lostark_parser.parse_get_jewelry_info(data),
-        "equipment": lostark_parser.parse_get_equipment_info(data),
-        "week_gold": lostark_parser.parse_get_week_gold_info(data),
-        "accessories": lostark_parser.parse_get_accessories_info(data),
-        "skill": lostark_parser.parse_get_skill_info(data),
-        "collections": lostark_parser.parse_get_collections(data),
-    }
+    return f"""
+{lostark_parser.parse_title(data)} | {lostark_parser.parse_name(data)} 
+
+직업 : {lostark_parser.parse_class(data)}
+템/전/원 : {lostark_parser.parse_item_level(data)} / {lostark_parser.parse_character_level(data)} / {lostark_parser.parse_expedition_level(data)} 
+서버/길드 : {lostark_parser.parse_server(data)} / {lostark_parser.parse_class(data)} 
+전투 특성 : {lostark_parser.parse_engrave(data)} 
+스킬포인트 : {lostark_parser.parse_skill_point(data)} 
+각인 : {lostark_parser} 
+공격력/체력 : {lostark_parser.parse_attack(data)} / {lostark_parser.parse_health(data)} 
+카드 : {lostark_parser.parse_card(data)} 
+섬마: {lostark_parser.parse_island_heart(data)} 미술품: {lostark_parser.parse_art(data)} 심장: {lostark_parser.parse_giant_heart(data)} 별: {lostark_parser.parse_start(data)} 
+모코코: {lostark_parser.parse_mokoko(data)} 모험물: {lostark_parser.parse_a_sailing_adventure(data)} 징표: {lostark_parser.parse_Signs_of_Ignaea(data)} 잎: {lostark_parser.parse_the_leaves_of_the_world_water(data)} 
+"""
 
 
-def get_character_info(username):
+def get_sub_character(username):
     res = requests.get('http://152.70.248.4:5000/userinfo/' + username)
     data = res.json()
-    return {
-        "basic": lostark_parser.parse_basic(data),
-        "level": lostark_parser.parse_level(data),
-        "sub_character": lostark_parser.parse_sub_character(data),
-        "stat": lostark_parser.parse_stat(data),
-        "engrave": lostark_parser.parse_engrave(data),
-        "ability_stone": lostark_parser.parse_ability_stone(data)
-    }
+    return lostark_parser.parse_sub_character(data)
 
 
-def get_jewelry_info(username):
+def get_jewelry(username):
     res = requests.get('http://152.70.248.4:5000/userinfo/' + username)
     data = res.json()
     return lostark_parser.parse_get_jewelry_info(data)
 
 
-def get_equipment_info(username):
+def get_equipment(username):
     res = requests.get('http://152.70.248.4:5000/userinfo/' + username)
     data = res.json()
     return lostark_parser.parse_get_equipment_info(data)
 
 
-def get_week_gold_info(username):
-    res = requests.get('http://152.70.248.4:5000/userinfo/' + username)
-    data = res.json()
-    return lostark_parser.parse_get_week_gold_info(data)
-
-
-def get_accessories_info(username):
+def get_accessories(username):
     res = requests.get('http://152.70.248.4:5000/userinfo/' + username)
     data = res.json()
     return lostark_parser.parse_get_accessories_info(data)
 
 
-def get_skill_info(username):
+def get_week_gold(username):
+    res = requests.get('http://152.70.248.4:5000/userinfo/' + username)
+    data = res.json()
+    return lostark_parser.parse_get_week_gold_info(data)
+
+
+def get_skill(username):
     res = requests.get('http://152.70.248.4:5000/userinfo/' + username)
     data = res.json()
     return lostark_parser.parse_get_skill_info(data)
-
-
-def get_collections(username):
-    res = requests.get('http://152.70.248.4:5000/userinfo/' + username)
-    data = res.json()
-    return lostark_parser.parse_get_collections(data)
--- api/lostark_parser.py
+++ api/lostark_parser.py
@@ -1,7 +1,7 @@
 import locale
 import re
 
-nl = ' | '
+nl = ''
 
 
 def parse_get_skill_info(data: dict):
@@ -10,16 +10,80 @@ def parse_get_skill_info(data: dict):
 """
 
 
-def parse_basic(data: dict):
-    return f"""
-서버 : {data["Basic"]["Server"]} | 클래스 : {data["Basic"]["Class"]["Name"]} | 길드 : {data["Basic"]["Guild"]} | 칭호 : {data["Basic"]["Title"]} |
-"""
+def parse_server(data: dict):
+    return f"""{data["Basic"]["Server"]}"""
 
 
-def parse_level(data: dict):
-    return f"""
-캐릭터 : {data["Basic"]["Level"]["Battle"]} | 아이템 : {data["Basic"]["Level"]["Item"]} | 원정대 : {data["Basic"]["Level"]["Expedition"]} | 영지 : {data["Basic"]["Wisdom"]["Level"]} | 
-        """
+def parse_class(data: dict):
+    return f"""{data["Basic"]["Class"]["Name"]}"""
+
+
+def parse_guild(data: dict):
+    return f"""{data["Basic"]["Guild"]}"""
+
+
+def parse_title(data: dict):
+    return f"""{data["Basic"]["Title"]}"""
+
+
+def parse_name(data: dict):
+    return f"""{data["Basic"]["Name"]}"""
+
+
+def parse_character_level(data: dict):
+    return f"""{data["Basic"]["Level"]["Battle"]}"""
+
+
+def parse_item_level(data: dict):
+    return f"""{data["Basic"]["Level"]["Item"]}"""
+
+
+def parse_skill_point(data: dict):
+    return f"""{data["Basic"]["SkillPoint"]["Used"]}/{data["Basic"]["SkillPoint"]["Total"]}"""
+
+
+def parse_expedition_level(data: dict):
+    return f"""{data["Basic"]["Level"]["Expedition"]}"""
+
+
+def parse_wisdom_level(data: dict):
+    return f"""{data["Basic"]["Wisdom"]["Level"]}"""
+
+
+def parse_attack(data: dict):
+    return f"""{data["Basic"]["Stat"]["Attack"]}"""
+
+
+def parse_card(data: dict):
+    return f"""{list(data["Basic"]["Card"])[-1]}"""
+
+
+def parse_health(data: dict):
+    return f"""{data["Basic"]["Stat"]["Health"]}"""
+
+
+def parse_critical(data: dict):
+    return f"""{data["Basic"]["Stat"]["Critical"]}"""
+
+
+def parse_specialty(data: dict):
+    return f"""{data["Basic"]["Stat"]["Specialty"]}"""
+
+
+def parse_agility(data: dict):
+    return f"""{data["Basic"]["Stat"]["Agility"]}"""
+
+
+def parse_proficiency(data: dict):
+    return f"""{data["Basic"]["Stat"]["Proficiency"]}"""
+
+
+def parse_endurance(data: dict):
+    return f"""{data["Basic"]["Stat"]["Endurance"]}"""
+
+
+def parse_engrave(data: dict):
+    return f"""{nl.join(data["Basic"]["Engrave"])}"""
 
 
 def parse_sub_character(data: dict):
@@ -29,28 +93,42 @@ def parse_sub_character(data: dict):
         """
 
 
-def parse_stat(data: dict):
-    return f"""
-공격력 : {data["Basic"]["Stat"]["Attack"]} | 최대 생명력 : {data["Basic"]["Stat"]["Health"]} | 치명 : {data["Basic"]["Stat"]["Critical"]} | 특화 : {data["Basic"]["Stat"]["Specialty"]} | 신속 : {data["Basic"]["Stat"]["Agility"]} | 제압 : {data["Basic"]["Stat"]["Proficiency"]} | 인내 : {data["Basic"]["Stat"]["Endurance"]} | 
-        """
-
-
-def parse_engrave(data: dict):
-    return f"""
-{nl.join(data["Basic"]["Engrave"])}
-        """
-
-
 def parse_ability_stone(data: dict):
     return f"""
 {nl.join(map(lambda ability: f"[{ability['EngraveName']}] 활성도 {ability['Effect']}", data["Items"]["어빌리티 스톤"]["Engrave"]))}
         """
 
 
-def parse_get_collections(data: dict):
-    return f"""
-섬의 마음 : {data["Collections"][0]["섬의 마음"]} {parse_collection_complete(data["Collections"][0]["섬의 마음"])} |  오르페우스의 별 : {data["Collections"][1]["오르페우스의 별"]} {parse_collection_complete(data["Collections"][1]["오르페우스의 별"])} |  거인의 심장 : {data["Collections"][2]["거인의 심장"]} {parse_collection_complete(data["Collections"][2]["거인의 심장"])} |  위대한 미술품 : {data["Collections"][3]["위대한 미술품"]} {parse_collection_complete(data["Collections"][3]["위대한 미술품"])} |  모코코 씨앗 : {data["Collections"][4]["모코코 씨앗"]} {parse_collection_complete(data["Collections"][4]["모코코 씨앗"])} |  항해 모험물 : {data["Collections"][5]["항해 모험물"]} {parse_collection_complete(data["Collections"][5]["항해 모험물"])} |  이그네아의 징표 : {data["Collections"][6]["이그네아의 징표"]} {parse_collection_complete(data["Collections"][6]["이그네아의 징표"])} |  세계수의 잎 : {data["Collections"][7]["세계수의 잎"]} {parse_collection_complete(data["Collections"][7]["세계수의 잎"])} |  
-"""
+def parse_island_heart(data: dict):
+    return f"""{data["Collections"][0]["섬의 마음"]} {parse_collection_complete(data["Collections"][0]["섬의 마음"])}"""
+
+
+def parse_start(data: dict):
+    return f"""{data["Collections"][1]["오르페우스의 별"]} {parse_collection_complete(data["Collections"][1]["오르페우스의 별"])}"""
+
+
+def parse_giant_heart(data: dict):
+    return f"""{data["Collections"][2]["거인의 심장"]} {parse_collection_complete(data["Collections"][2]["거인의 심장"])}"""
+
+
+def parse_art(data: dict):
+    return f"""{data["Collections"][3]["위대한 미술품"]} {parse_collection_complete(data["Collections"][3]["위대한 미술품"])}"""
+
+
+def parse_mokoko(data: dict):
+    return f"""{data["Collections"][4]["모코코 씨앗"]} {parse_collection_complete(data["Collections"][4]["모코코 씨앗"])}"""
+
+
+def parse_a_sailing_adventure(data: dict):
+    return f"""{data["Collections"][5]["항해 모험물"]} {parse_collection_complete(data["Collections"][5]["항해 모험물"])}"""
+
+
+def parse_Signs_of_Ignaea(data: dict):
+    return f"""{data["Collections"][6]["이그네아의 징표"]} {parse_collection_complete(data["Collections"][6]["이그네아의 징표"])}"""
+
+
+def parse_the_leaves_of_the_world_water(data: dict):
+    return f"""{data["Collections"][7]["세계수의 잎"]} {parse_collection_complete(data["Collections"][7]["세계수의 잎"])}"""
 
 
 def parse_collection_complete(data: dict):
@@ -65,40 +143,40 @@ def parse_jewelry_to_string(jewl):
 
 
 def parse_get_jewelry_info(data: dict):
-    return " | ".join(list(map(parse_jewelry_to_string, data["Jewl"])))
+    return "".join(list(map(parse_jewelry_to_string, data["Jewl"])))
 
 
 def parse_get_equipment_info(data: dict):
     result = ""
     try:
-        result += f"무기 : {data['Items']['무기']['Name']} ({data['Items']['무기']['Quality']}) | "
+        result += f"무기 : {data['Items']['무기']['Name']} ({data['Items']['무기']['Quality']})"
     except KeyError:
-        result += "무기 : 없음 | "
+        result += "무기 : 없음"
 
     try:
-        result += f"투구 : {data['Items']['머리 방어구']['Name']} ({data['Items']['머리 방어구']['Quality']}) | "
+        result += f"투구 : {data['Items']['머리 방어구']['Name']} ({data['Items']['머리 방어구']['Quality']})"
     except KeyError:
-        result += "투구 : 없음 | "
+        result += "투구 : 없음"
 
     try:
-        result += f"견갑 : {data['Items']['어깨 방어구']['Name']} ({data['Items']['어깨 방어구']['Quality']}) | "
+        result += f"견갑 : {data['Items']['어깨 방어구']['Name']} ({data['Items']['어깨 방어구']['Quality']})"
     except KeyError:
-        result += "견갑 : 없음 | "
+        result += "견갑 : 없음"
 
     try:
-        result += f"상의 : {data['Items']['상의']['Name']} ({data['Items']['상의']['Quality']}) | "
+        result += f"상의 : {data['Items']['상의']['Name']} ({data['Items']['상의']['Quality']})"
     except KeyError:
-        result += "상의 : 없음 | "
+        result += "상의 : 없음"
 
     try:
-        result += f"하의 : {data['Items']['하의']['Name']} ({data['Items']['하의']['Quality']}) | "
+        result += f"하의 : {data['Items']['하의']['Name']} ({data['Items']['하의']['Quality']})"
     except KeyError:
-        result += "하의 : 없음 | "
+        result += "하의 : 없음"
 
     try:
-        result += f"장갑 : {data['Items']['장갑']['Name']} ({data['Items']['장갑']['Quality']}) | "
+        result += f"장갑 : {data['Items']['장갑']['Name']} ({data['Items']['장갑']['Quality']})"
     except KeyError:
-        result += "장갑 : 없음 | "
+        result += "장갑 : 없음"
 
     return result
 
@@ -108,14 +186,14 @@ def parse_get_week_gold_info(data: dict):
 
     gold_list = data["Gold"]["GoldList"]
     character = lambda \
-            gold: f"{gold['Name']} {format(parse_week_gold_calculator(parse_level_str_to_int(gold['Level'])), ',d')} |  + {gold['Class']} {gold['Level']} |  "
+            gold: f"{gold['Name']} {format(parse_week_gold_calculator(parse_level_str_to_int(gold['Level'])), ',d')} + {gold['Class']} {gold['Level']} "
 
     total_gold = 0
     for gold in gold_list:
         total_gold += parse_week_gold_calculator(
             int(gold['Level'].replace('Lv.', '').replace(',', '').replace('.', '')) / 100)
 
-    character_list = " | ".join(list(map(character, gold_list)))
+    character_list = "".join(list(map(character, gold_list)))
     return f"""
 {character_list} 
 
@@ -160,27 +238,27 @@ def parse_get_accessories_info(data: dict):
     result = ""
 
     try:
-        result += f"반지 : {data['Items']['반지1']['Name']} {data['Items']['반지1']['Plus']} | "
+        result += f"반지 : {data['Items']['반지1']['Name']} {data['Items']['반지1']['Plus']}"
     except KeyError:
         result += f"반지 : 없음"
 
     try:
-        result += f"반지 : {data['Items']['반지2']['Name']} {data['Items']['반지2']['Plus']} | "
+        result += f"반지 : {data['Items']['반지2']['Name']} {data['Items']['반지2']['Plus']}"
     except KeyError:
         result += f"반지 : 없음"
 
     try:
-        result += f"목걸이 : {data['Items']['목걸이']['Name']} {data['Items']['목걸이']['Plus']} | "
+        result += f"목걸이 : {data['Items']['목걸이']['Name']} {data['Items']['목걸이']['Plus']}"
     except KeyError:
         result += f"목걸이 : 없음"
 
     try:
-        result += f"귀걸이 : {data['Items']['귀걸이1']['Name']} {data['Items']['귀걸이1']['Plus']} | "
+        result += f"귀걸이 : {data['Items']['귀걸이1']['Name']} {data['Items']['귀걸이1']['Plus']}"
     except KeyError:
         result += f"귀걸이 : 없음"
 
     try:
-        result += f"귀걸이 : {data['Items']['귀걸이2']['Name']} {data['Items']['귀걸이2']['Plus']} | "
+        result += f"귀걸이 : {data['Items']['귀걸이2']['Name']} {data['Items']['귀걸이2']['Plus']}"
     except KeyError:
         result += f"귀걸이 : 없음"
 
@@ -195,4 +273,4 @@ def parse_get_accessories_info(data: dict):
 
 def parse_bracelet_option_to_string(data: dict):
     hyphen = lambda s: " - " + s
-    return " | ".join(list(map(hyphen, data["Items"]["팔찌"]["Plus"])))
+    return "".join(list(map(hyphen, data["Items"]["팔찌"]["Plus"])))
