24fe7c327ab05cfa833a1a5d05ae84207fb05481
donghune <reona7140@naver.com>
2022-07-15 18:40:00 +0900

    버그수정

--- app.py
+++ app.py
@@ -9,22 +9,23 @@ CORS(app, resources={r'*': {'origins': 'http://152.70.248.4'}})
 
 @app.route("/cmd/<string:args>")
 def cmd(args):
-    if args == "!명령어":
+    c = args.split(' ')
+    if c[0] == "!명령어":
         return "!정보 | !부캐 | !장비 | !보석 | !악세 | !주간골드 | !스킬"
-    elif args[0] == "!정보":
-        return lostark.get_basic(args[1])
-    elif args[0] == "!부캐":
-        return lostark.get_sub_character(args[1])
-    elif args[0] == "!장비":
-        return lostark.get_jewelry(args[1])
-    elif args[0] == "!보석":
-        return lostark.get_equipment(args[1])
-    elif args[0] == "!악세":
-        return lostark.get_accessories(args[1])
-    elif args[0] == "!주간골드":
-        return lostark.get_week_gold(args[1])
-    elif args[0] == "!스킬":
-        return lostark.get_skill(args[1])
+    elif c[0] == "!정보":
+        return lostark.get_basic(c[1])
+    elif c[0] == "!부캐":
+        return lostark.get_sub_character(c[1])
+    elif c[0] == "!장비":
+        return lostark.get_jewelry(c[1])
+    elif c[0] == "!보석":
+        return lostark.get_equipment(c[1])
+    elif c[0] == "!악세":
+        return lostark.get_accessories(c[1])
+    elif c[0] == "!주간골드":
+        return lostark.get_week_gold(c[1])
+    elif c[0] == "!스킬":
+        return lostark.get_skill(c[1])
     else:
         return "알 수 없는 명령어 입니다."
 
