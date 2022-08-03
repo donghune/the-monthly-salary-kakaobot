04b7a5bd62cb995d8584807064fe8e06bce8dc2e
donghune <reona7140@naver.com>
2022-08-03 22:37:01 +0900

    ㅁㄴㅇ

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ CORS(app, resources={r'*': {'origins': 'http://lostarkapi.ga'}})
 
 @app.route("/cmd/<string:args>")
 def cmd(args):
-    c = args.split(' ')
+    c = args.split('%20')
     if c[0] == "!명령어":
         return "!정보 | !부캐 | !장비 | !보석 | !악세 | !주간골드 | !스킬"
     elif c[0] == "!정보":
