d0279c3db98ba8de34c99a2ff044f2cc953c5555
donghune <reona7140@naver.com>
2022-08-03 23:01:23 +0900

    ㅁㄴㅇ

--- api/lostark_parser.py
+++ api/lostark_parser.py
@@ -100,15 +100,15 @@ def parse_ability_stone(data: dict):
 
 
 def parse_island_heart(data: dict):
-    return f"""{data["Collections"][0]["섬의 마음"]}"""
+    return f"""{data["Collections"][1]["섬의 마음"]}"""
 
 
 def parse_start(data: dict):
-    return f"""{data["Collections"][1]["오르페우스의 별"]}"""
+    return f"""{data["Collections"][7]["오르페우스의 별"]}"""
 
 
 def parse_giant_heart(data: dict):
-    return f"""{data["Collections"][2]["거인의 심장"]}"""
+    return f"""{data["Collections"][0]["거인의 심장"]}"""
 
 
 def parse_art(data: dict):
@@ -116,11 +116,11 @@ def parse_art(data: dict):
 
 
 def parse_mokoko(data: dict):
-    return f"""{data["Collections"][4]["모코코 씨앗"]}"""
+    return f"""{data["Collections"][2]["모코코 씨앗"]}"""
 
 
 def parse_a_sailing_adventure(data: dict):
-    return f"""{data["Collections"][5]["항해 모험물"]}"""
+    return f"""{data["Collections"][4]["항해 모험물"]}"""
 
 
 def parse_Signs_of_Ignaea(data: dict):
@@ -128,7 +128,7 @@ def parse_Signs_of_Ignaea(data: dict):
 
 
 def parse_the_leaves_of_the_world_water(data: dict):
-    return f"""{data["Collections"][7]["세계수의 잎"]}"""
+    return f"""{data["Collections"][5]["세계수의 잎"]}"""
 
 
 def parse_jewelry_to_string(jewl):
